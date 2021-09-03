5bdf851296696c8f48b584579459e2d5f8acd7d9
llauros <lucaslauro@live.com>
2021-09-03 13:14:42 -0300

    Deploy inicial - Blog Pessoal

---
+++ src/main/java/com/generation/myblog/model/Usuario.java
@@ -0,0 +1,118 @@
+package com.generation.myblog.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Classe Modelo para Usuario , a mesma é considerada uma entidade no banco de
+ * Dados
+ * 
+ * @since 1.0
+ * @author llauro
+ *
+ */
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Necessario Nome")
+	private String nome;
+
+	@NotBlank(message = "Necessario Email")
+	@Email(message = "Necessario Email")
+	private String email;
+
+	@Size(min = 5, max = 100, message = "Necessario min 5 caracteres")
+	private String senha; // Necessarioamente Size deve ser max 100
+
+	private String foto;
+
+	private String tipo;
+
+	@OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({ "criador" })
+	private List<Postagem> minhasPostagens = new ArrayList<>();
+
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Long id, String nome, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Postagem> getMinhasPostagens() {
+		return minhasPostagens;
+	}
+
+	public void setMinhasPostagens(List<Postagem> minhasPostagens) {
+		this.minhasPostagens = minhasPostagens;
+	}
+}
