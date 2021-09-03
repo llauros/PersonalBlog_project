e36f43afaa1e7ffde2557d7a321a46ada8cab9ba
llauros <lucaslauro@live.com>
2021-09-03 10:40:34 -0300

    fix: Consertado Problama de log de inicialização

---
+++ myblog-API/src/main/java/com/generation/myblog/MyblogApiApplication.java
@@ -0,0 +1,13 @@
+package com.generation.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogApiApplication.class, args);
+	}
+
+}
